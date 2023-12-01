56493cdda55216846a23849a030e3d519fa42fa1
Andrea Lucero <alucerotrasvina@gmail.com>
2023-12-01 13:16:53 -0700

    Modelo categoria

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nom_categoria=models.CharField(max_length=100)
+    
+    class Meta:
+        db_table='categorias'
